652d25712a4830ea38712c02da5b70276eb83869
Mariana <marianacifuenteszu@gmail.com>
2023-03-30 11:47:28 -0500

    adding-play-button

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import {
   UnstyledButton,
   useMantineTheme,
 } from "@mantine/core";
-import { PlayerPlay, Settings, Playlist, Plus } from "tabler-icons-react";
+import { PlayerPlay, Settings, Playlist, Plus, PlaylistAdd } from "tabler-icons-react";
 
 export default function Home() {
   const theme = useMantineTheme();
@@ -18,7 +18,7 @@ export default function Home() {
         style={{
           backgroundColor: theme.colors.gray[0],
           width: "500px",
-          height: "100vh",
+          height: "10vh", //antes estaba en 100vh
         }}
         mx="auto"
         direction="column"
@@ -28,7 +28,39 @@ export default function Home() {
             <Settings />
           </ActionIcon>
           <ActionIcon color="gray" size="xl" m="sm" variant="transparent">
-            <Playlist />
+            <PlaylistAdd />
+          </ActionIcon>
+        </Group>
+        <Group
+          spacing="none"
+          mt="sm"
+          position="center"
+          mx="auto"
+          direction="column"
+          height="175px"
+        >
+          <Title order={1} style={{ fontWeight: "900", fontSize: "4rem" }}>
+            25:00
+          </Title>
+          <Text>Record Mantine Tutorial</Text>
+          {/* Esto debería estar debajo del contador */}
+        </Group>
+        <Group
+          mt="sm"
+          position="center"
+          mx="auto"
+          direction="row"
+          height="200px"
+          mb="md"
+        >
+          <ActionIcon color="dark" size="xl" variant="transparent">
+            -5
+          </ActionIcon>
+          <ActionIcon radius="xl" color="dark" size="xl" variant="filled">
+            <PlayerPlay />
+          </ActionIcon>
+          <ActionIcon color="dark" size="xl" variant="transparent">
+            +5
           </ActionIcon>
         </Group>
       </Group>
